f86c391e7082a0a42737c63f0dea153988b775d2
Abdullah Al Noman <imnomanmohammad19@gmail.com>
2024-07-07 22:41:28 +0600

    First Commit of Tokens
    Explaining Tokens in C Programming

---
+++ tokens.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n Tokens in C Programming \n");
+    printf("In the C programming language,\n tokens are the smallest units of a program \n that are meaningful to the compiler. \n");
+    return 0;
+}
+//TODO Remember: In total, there are six types of tokens in C programming:-
+//? 1. Keywords: Reserved words that have special meaning in C (e.g., int, return, if).
+//? 2. Identifiers: Names given to variables, functions, arrays, etc. (e.g., main, sum, myVar).
+//? 3. Constants: Fixed values that do not change (e.g., 10, 3.14, 'a').
+//? 4. String literals: Sequence of characters enclosed in double quotes (e.g., "Hello, World!").
+//? 5. Operators: Symbols that perform operations on variables and values (e.g., +, -, *, /)
+//? 6. Punctuation/Special Symbols: Symbols that separate tokens and define the structure (e.g., ;, ,, {, }).
